ffca924210d91b5fad61dbfe69cacd030b515116
MaciejReimann <maciej.reimann@gmail.com>
2018-10-28 20:03:08 +0100

    init() added and tested

--- public/__tests__/Tetris.test.js.js
+++ public/__tests__/Tetris.test.js.js
@@ -1,124 +1,140 @@
-
 // Testing tetris API
 const tetris = require('../scripts/Tetris');
 
-// Board setup constants
-const width = 10;
-const height = 20;
-const tempo = 1000;
-const startPoint = {x: width / 2, y: 0};
+// Board default setup constants
+const defaultBoard = {
+    width: 10,
+    height: 20,
+    tempo: 1000,
+    step: 1,
+    startPoint: {x: 5, y: 0}
+};
 
-// getState() and setUp()
-beforeEach(_ => {
-        tetris.setUp(width, height, tempo);
+describe("Initial state for default setup", () => {
+    const { width, height, tempo, step, startPoint } = defaultBoard;    
+    beforeEach(() => {        
+        tetris.init(width, height, tempo, step);
+    });
+    
+    test("Width setup", () => {
+        expect(tetris.getBoard().width).toBe(width)
+    });
+    test("Height setup", () => {
+        expect(tetris.getBoard().height).toBe(height)
+    });
+    test("Tempo setup", () => {
+        expect(tetris.getBoard().tempo).toBe(tempo)
+    });
+    test("Step setup", () => {
+        expect(tetris.getBoard().step).toBe(step)
+    });
+    test("Startpoint setup", () => {
+        expect(tetris.getBoard().startPoint).toEqual(startPoint)
+    });
 })
 
-test("Width setup", () => {    
-    expect(tetris.getBoard().width).toBe(width)
-})
 
-test("Height setup", () => {    
-    expect(tetris.getBoard().height).toBe(height)
-})
 
-test("Tempo setup", () => {    
-    expect(tetris.getBoard().tempo).toBe(tempo)
-})
 
-test("Startpoint setup", () => {    
-    expect(tetris.getBoard().startPoint).toEqual(startPoint)
-})
+
 
 // start()
-test("Game status before start", () => {
-    expect(tetris.getState().gameStarted).toBeFalsy()
-})
-test("Game status after start", () => {
-    tetris.start()  
-    expect(tetris.getState().gameStarted).toBeTruthy()
-})
+// test("Game status before start", () => {
+//     expect(tetris.getState().gameStarted).toBeFalsy()
+// })
+// test("Game status after start", () => {
+//     tetris.start()  
+//     expect(tetris.getState().gameStarted).toBeTruthy()
+// })
 
 // moveDown()
-test("Pivot moved from start n times", () => {
-    let moveCounter = 0;
-    tetris.start();
-    do {        
-        if(moveCounter === 0) {
-            expect(tetris.getStatus()).toEqual("Game started");
-            expect(tetris.getState().pivot).toEqual(startPoint);
-        } else {
-            const movedPivot = {x: width / 2, y: moveCounter};
-            tetris.moveDown(1);
-            expect(tetris.getStatus()).toEqual("Moved down");
-            expect(tetris.getState().pivot).toEqual(movedPivot);            
-        }        
-        moveCounter ++;   
-    } while (moveCounter < height)
-})
+// test("Pivot moved from start n times", () => {
+//     let moveCounter = 0;
+//     tetris.start();
+//     do {        
+//         if(moveCounter === 0) {
+//             expect(tetris.getStatus()).toEqual("Game started");
+//             expect(tetris.getState().pivot).toEqual(startPoint);
+//         } else {
+//             const movedPivot = {x: width / 2, y: moveCounter};
+//             tetris.moveDown(1);
+//             expect(tetris.getStatus()).toEqual("Moved down");
+//             expect(tetris.getState().pivot).toEqual(movedPivot);            
+//         }        
+//         moveCounter ++;   
+//     } while (moveCounter < height)
+// })
 
-test("Pivot moved from start n-height times, squares.length increments at height -1", () => {
-    let max = 20
-    let nCounter = 1
-    let moveCounter = 1;
-    
-    while (moveCounter < height * max) {
-        let squaresCounterBeforeMove = tetris.getState().squares.length;
-        tetris.moveDown(1);
-        let squaresCounterAfterMove = tetris.getState().squares.length;
-        if(tetris.getState().pivot.y === height - 1) {            
-            expect(squaresCounterAfterMove - squaresCounterBeforeMove).toBe(1)
-            expect(squaresCounterAfterMove).toBe(nCounter)
-            nCounter++
-        }
-        moveCounter ++;   
-    } 
-})
+// test("Pivot moved from start n-height times, squares.length increments at height", () => {
+//     let max = 20
+//     let nCounter = 1
+//     let moveCounter = 1;
+//     tetris.start();
+//     while (moveCounter < height * max) {
+//         let squaresCounterBeforeMove = tetris.getState().squares.length;
+//         tetris.moveDown(1);
+//         let squaresCounterAfterMove = tetris.getState().squares.length;
+//         if(tetris.getState().pivot.y === height) {            
+//             expect(squaresCounterAfterMove - squaresCounterBeforeMove).toBe(1)
+//             expect(squaresCounterAfterMove).toBe(nCounter)
+//             nCounter ++
+//         }
+//         moveCounter ++;
+        
+//     }
+//     console.log(nCounter) 
+// })
+
+// const sumArProgression = (n, a1, an) => n * (a1 + an) / 2;
+
+// test("Game over", () => {
+//     const endCounter = sumArProgression(height + 1, height, 0)
+//     let max = 100;
+//     let moveCounter = 0;
+//     tetris.start();
+//     do {
+//         tetris.moveDown(1);
+//         if (tetris.getStatus() === "Game over") {
+//             expect(tetris.getState().squares.length).toBe(height)
+//             expect(moveCounter + 1).toBe(endCounter)
+//             break //209
+//         }
+//         moveCounter ++;
+//     } while (moveCounter < height * max)
+// })
+
+// test("Game over one move after board is full", () => {
+//     let max = 100;
+//     let moveCounter = 0;
+//     tetris.start();
+//     do {
+//         tetris.moveDown(1);
+//         if(tetris.getState().squares.length === height) {
+//             tetris.moveDown(1);
+//             expect(tetris.getStatus()).toEqual("Game over")
+//             break
+//         }
+//         moveCounter ++;
+//     } while (moveCounter < height * max)
+// })
+
+
+// test('test game over after some time has passed', done => {
+//     let timer = 0;
+//     const tempo = 1000;
+//     tetris.start();
+//     expect(tetris.getStatus()).toEqual("Game started");
+//     setInterval( 
+//         () => {
+//             timer ++
+//             if (timer === 210) {
+//                 expect(tetris.getStatus()).toEqual("Game over");
+//                 done();
+//             }
+//         },
+//         tempo 
+//     );
+// })
 
 
 
-test("Game over", () => {
-    const sumArProgression = (n, a1, an) => n * (a1 + an) / 2;
-    const endCounter = sumArProgression(height + 1, height, 0)
-    let moveCounter = 0;
-    tetris.start();
-    while (true) {
-        tetris.moveDown(1);
-        if (tetris.getStatus() === "Game over") {
-            expect(tetris.getState().squares.length).toBe(height)
-            expect(moveCounter + 1).toBe(endCounter)
-            break //209
-        }
-        moveCounter ++;
-    } 
-})
-
-test("Game over one move after board is full", () => {
-    let max = 100;
-    let moveCounter = 0;
-    tetris.start();
-    do {
-        tetris.moveDown(1);
-        if(tetris.getState().squares.length === height) {
-            tetris.moveDown(1);
-            expect(tetris.getStatus()).toEqual("Game over")
-            break
-        }
-        moveCounter ++;
-    } while (moveCounter < height * max)
-})
-
-test('test game self ending', () => {
-    tetris.start();
-    let timer = 0;
-    const tempo = 3000;
-
-    const movedPivot = {x: width / 2, y: timer};
-
-    setInterval( 
-        () => {
-            timer ++;
-            console.log(`timer is ${timer}`);
-        },
-        tempo
-    );
-});
--- public/scripts/Tetris.js
+++ public/scripts/Tetris.js
@@ -1,37 +1,55 @@
 
 
-// const { 
-//     merge,
-// } = require('./helpers/pointsManipulation');
+const { 
+    createPoint,
+} = require('./helpers/pointsHelpers');
 
-const Point = require('./helpers/Point');
+// Create empty object to store the data in;
+let board = {};
+let state = {};
 
-// Tetris object to be exported
-
-const  board = {};
-const state = {};
-const status = '';
-
+// Initialize board with given board parameters and initial game state;
+function init(width, height, tempo, step) {
+    board = {
+        width,
+        height,
+        tempo,
+        step,
+        startPoint: {x: width / 2, y: 0}
+    };
+    state = {
+        gameStarted: false,
+        gameIsOver: false,
+        pivot: board.startPoint,
+        squares: []
+    };
+};
 
 // PRIVATE METHODS
-// funcion definnition
+function movePoint(point, x, y) {
+    return createPoint(point.x + x, point.y + y)
+}
+
+function movePointDown(point, y) {
+    return movePoint(point, 0, y)
+}
+
+ 
+
 const arePointsEqual = (p1, p2) => p1.x === p2.x && p1.y === p2.y;
 
 const isOnStart = position => position.y === board.startPoint.y;
 
 // Check if moved doesn't hit other squares on the board;
-let hitsOthers = (position, n, others) => others.some(square => 
+const hitsOthers = (position, n, others) => others.some(square =>
     arePointsEqual(square, {x: position.x, y: position.y + n})
 );
 
-
-
-// Check if moved doesn't get outside the board;
+// Check if moved point doesn't get outside the board;
 const hitsBottom = (position, n) => position.y + n >= board.height;
 // Check if can be moved down;
-const canMoveDown = (position, n, others) => {
-    return !hitsBottom(position, n) && !hitsOthers(position, n, others);
-}
+const canMoveDown = (position, n, others) => 
+    !hitsBottom(position, n) && !hitsOthers(position, n, others);
 
 const getStartPosition = () => board.startPoint;
 
@@ -45,53 +63,25 @@ const getNextPosition = (position, n) => {
 // PUBLIC METHODS
 const getBoard = () => board;
 const getState = () => state;
-const getStatus = () => status;
 
-const setUp = (width, height, tempo) => {
-    board = {
-        width,
-        height,
-        tempo,
-        startPoint: {x: width / 2, y: 0}
-    }
-    return state;
-}
-
-// rozbić start na init (ustawienie planszy) i start - wystartowanie gry.
 const start = () => {
-    status = "Game started";
-    state = {
-        gameStarted: true,
-        gameIsOver: false,
-        pivot: board.startPoint,
-        squares: []
-    }
-    
-    setInterval(() => nextPositon(1), board.tempo);
-
-    return state;
+    state.gameStarted = true;    
+    // setInterval(() => moveDown(1), board.tempo)
 }
-
-
 
 const pause = () => {
-    status = "Game started";
     state.gamePaused = true;
-
-    return state;
 }
 
-
-
-const nextPositon = (n) => {
-    if(!canMoveDown(state.pivot, n, state.squares)) {
+function moveDown(n) {
+    if(canMoveDown(state.pivot, n, state.squares)) {
+        status = "Moved down";
         state.pivot = getNextPosition(state.pivot, n);
-
-        state.square = state.square.concat(state.pivot);
         state.gameIsOver = false;
     } else {
-        start.gameIsOver = true;
-        state.pivot = getStartPosition();
+        state.squares.concat(state.pivot)
+        state.pivot = getStartPosition() 
+        state.gameIsOver = true;
     }
 }
 
@@ -108,15 +98,11 @@ const moveLeft = () => {
 module.exports = {
     getBoard,
     getState,
-    getStatus,
 
-    state,
-    hitsOthers,
-
-    setUp,
+    init,
     start,
     pause,
-    nextPositon,
+    moveDown,
     moveRight,
     moveLeft
 };
