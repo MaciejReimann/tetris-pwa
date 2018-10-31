d4b202174e78bbaca2e77d084dc6b2e0812d0ad3
MaciejReimann <maciej.reimann@gmail.com>
2018-10-31 11:37:50 +0100

    test refactored

--- public/__tests__/helpers/tetrominosHelpers.test.js
+++ public/__tests__/helpers/tetrominosHelpers.test.js
@@ -1,12 +1,13 @@
 const tetrominoStock = require('../../scripts/helpers/tetrominosHelpers');
 const { clone } = require('../../scripts/helpers/arrayHelpers');
 
-// const allTetrominos = ["T_tetromino", "I_tetromino", "C_tetromino"];
-
-test("Stock of 3 random tetrominos shown and on gettting first, the last one pops in", () => {
-    tetrominoStock.build(3);
-    const originalStock = clone(tetrominoStock.showCurrent());
-    expect(tetrominoStock.getFirstAndReplenish()).toEqual(originalStock[0])
-    expect(tetrominoStock.getFirstAndReplenish()).toEqual(originalStock[1])
-    expect(tetrominoStock.getFirstAndReplenish()).toEqual(originalStock[2])
+test("From a stock of n tetrominos is generated, get first and replenish the stock", () => {
+    const n = 100;
+    let counter = 0;
+    tetrominoStock.build(n);
+    const originalStock = clone(tetrominoStock.getCurrent());
+    while (counter < n) {
+        expect(tetrominoStock.getFirstAndReplenish()).toEqual(originalStock[counter]);
+        counter ++;
+    };
 });
