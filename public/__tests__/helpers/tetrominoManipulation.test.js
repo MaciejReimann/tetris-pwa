8808e559d44823499888e09d76a80364e11ca117
MaciejReimann <maciej.reimann@gmail.com>
2018-11-01 14:04:02 +0100

    tested and commented

--- public/__tests__/helpers/tetrominoManipulation.test.js
+++ public/__tests__/helpers/tetrominoManipulation.test.js
@@ -1,31 +1,78 @@
-const { 
-    getParallelSquareVertices,
+const {
     rotateTetromino,
     scaleTetromino,
     positionTetromino,
     getGlobalTetrominoLocation,
+} = require('../../scripts/helpers/tetrominoManipulation').test;
+const {
     getGlobalTetrominoVertices
 } = require('../../scripts/helpers/tetrominoManipulation');
 
+const {
+    isPoint,
+    movePointOnY, 
+    multiplyPoint,
+    addTwoPoints,
+} = require('../../scripts/helpers/pointHelpers');
+
 const tetrominoStock = require('../../scripts/helpers/tetrominoCreation');
+const I_tetromino = tetrominoStock.allTetrominos[1];
 
-const defaultAngle = 0;
-const defaultCenter = { x: 0, y: 0 };
+const defaulPIvot = { x: 0, y: 0 };
+const movedPivotRight = { x: 1, y: 0 };
+const movedPivotLeft = { x: -1, y: 0 };
 
-describe("Initial state for default setup", () => {
-    const randomTetris = tetrominoStock.build(1)[0];
-    const angle = defaultAngle;
-    const center = defaultCenter;
-    const dim = 2;
-    const parallelSquareVertices = [
-        { x: 1, y: 1 },
-        { x: -1, y: 1 },
-        { x: -1, y: -1 },
-        { x: 1, y: -1 }
-    ];
 
-    test("A parallel square", () => {
-        expect(getParallelSquareVertices(angle, center, dim))
-            .toEqual(parallelSquareVertices)
+describe("Tetromino manipulation", () => {
+    const tetrominoMovedOneUp = I_tetromino.map(squareCenter => 
+        movePointOnY(squareCenter, -1)).reverse();
+    const tetrominoScaledBy_2 = I_tetromino.map(squareCenter => 
+        multiplyPoint(squareCenter, 2));
+    const tetrominoScaledBy_Half = I_tetromino.map(squareCenter => 
+        multiplyPoint(squareCenter, .5));
+    const tetrominoMovedByOneRight = I_tetromino.map(squareCenter => 
+        addTwoPoints(squareCenter, movedPivotRight));
+    const tetrominoMovedByOneLeft = I_tetromino.map(squareCenter => 
+        addTwoPoints(squareCenter, movedPivotLeft));
+
+    test('Tetromino rotation', () => {
+        expect(rotateTetromino(I_tetromino, 360)).toEqual(I_tetromino);
+        expect(rotateTetromino(I_tetromino, 0)).toEqual(I_tetromino);
+        expect(rotateTetromino(I_tetromino, 180)).toEqual(tetrominoMovedOneUp);
     });
-})
+    test('Tetromino scaling', () => {
+        expect(scaleTetromino(I_tetromino, 1)).toEqual(I_tetromino);
+        expect(scaleTetromino(I_tetromino, 2)).toEqual(tetrominoScaledBy_2);
+        expect(scaleTetromino(I_tetromino, .5)).toEqual(tetrominoScaledBy_Half);
+    });
+    test('Position tetromino', () => {
+        expect(positionTetromino(I_tetromino, defaulPIvot)).toEqual(I_tetromino);
+        expect(positionTetromino(I_tetromino, movedPivotRight)).toEqual(tetrominoMovedByOneRight);
+        expect(positionTetromino(I_tetromino, movedPivotLeft)).toEqual(tetrominoMovedByOneLeft);
+    });
+    test('Position after manipulation', () => {
+        expect(getGlobalTetrominoLocation(I_tetromino, 360, 1, defaulPIvot)).toEqual(I_tetromino);
+        expect(getGlobalTetrominoLocation(I_tetromino, 0, 1, defaulPIvot)).toEqual(I_tetromino);
+        expect(getGlobalTetrominoLocation(I_tetromino, 0, 2, defaulPIvot)).toEqual(tetrominoScaledBy_2);
+        expect(getGlobalTetrominoLocation(I_tetromino, 180, 1, defaulPIvot)).toEqual(tetrominoMovedOneUp);
+        expect(getGlobalTetrominoLocation(I_tetromino, 0, 1, movedPivotRight)).toEqual(tetrominoMovedByOneRight);
+    });
+});
+
+describe("Tetromino vertices coordinates", () => {
+    test('Right data format', () => {
+        expect.anything(getGlobalTetrominoVertices(I_tetromino, 0, 1, defaulPIvot));
+        expect(
+            getGlobalTetrominoVertices(I_tetromino, 0, 1, defaulPIvot).length === 4
+        ).toBeTruthy();
+        expect(
+            getGlobalTetrominoVertices(I_tetromino, 0, 1, defaulPIvot).map(
+                item => item.length === 4
+        )).toBeTruthy();
+        expect(
+            getGlobalTetrominoVertices(I_tetromino, 0, 1, defaulPIvot).map(
+                item => isPoint(item)
+        )).toBeTruthy();
+    });
+});
+
--- public/scripts/helpers/tetrominoManipulation.js
+++ public/scripts/helpers/tetrominoManipulation.js
@@ -1,11 +1,19 @@
 const { 
    regularPolygon
-} = require('./regularPolygon.js');
+} = require('./regularPolygon');
+const {
+    rotatePointOnGlobalZero, 
+    multiplyPoint,
+    addTwoPoints
+} = require('./pointHelpers')
 
+// Parallel to local coordinates system, so as when tetromino is rotated 
+// the squares get rotated  as well according to the pivotLocation
 function getParallelSquareVertices(angle, center, dim) {
     return regularPolygon(angle + 45, center, 4, (dim / 2) * Math.sqrt(2));
 };
 
+// Tetromino here is defined as coordinates of centers of its four squares;
 function rotateTetromino(tetromino, angle) {
     return tetromino.map(squareCenter => rotatePointOnGlobalZero(squareCenter, angle))
 };
@@ -14,29 +22,40 @@ function scaleTetromino(tetromino, scale) {
     return tetromino.map(squareCenter => multiplyPoint(squareCenter, scale));
 };
 
-function positionTetromino(tetromino, pivot, ) {
-    return tetromino.map(squareCenter => addTwoPoints(squareCenter, pivot));
+function positionTetromino(tetromino, pivotLocation, ) {
+    return tetromino.map(squareCenter => addTwoPoints(squareCenter, pivotLocation));
 };
 
-function getGlobalTetrominoLocation(tetromino, angle, scale, pivot) {
+// Here we get the global position of tetromino's square centers;
+function getGlobalTetrominoLocation(tetromino, angle, scale, pivotLocation) {
     return (
         positionTetromino( 
             scaleTetromino( 
                 rotateTetromino(tetromino, angle), 
             scale ), 
-        pivot)
+            pivotLocation)
     )
 };
 
-function getGlobalTetrominoVertices(tetromino, angle, scale, pivot) {
-    
+// PUBLIC METHOD
+// And finally we receive the global position of eqch if it's square's vertices
+// in the form of array of four array of four point objects each;
+function getGlobalTetrominoVertices(tetromino, angle, scale, pivotLocation) {
+    return getGlobalTetrominoLocation(tetromino, angle, scale, pivotLocation)
+        .map(squareCenter => getParallelSquareVertices(angle, squareCenter, scale) );
 }
 
+// Publicly accessed 
 module.exports = {
+    getGlobalTetrominoVertices
+};
+
+// Exposed only for testing
+module.exports.test = {
     getParallelSquareVertices,
     rotateTetromino,
     scaleTetromino,
     positionTetromino,
     getGlobalTetrominoLocation,
-    getGlobalTetrominoVertices
-}
+};
+
