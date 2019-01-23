16593d85402bc215ac7299daa64d7cc43c394a86
MaciejReimann <maciej.reimann@gmail.com>
2019-01-23 18:44:19 +0100

    another try

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Set static folder
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "../public")));
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../public", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
