dce185bbb1b6d7c7c7e6379e114893719b878006
MaciejReimann <maciej.reimann@gmail.com>
2019-01-23 17:26:22 +0100

    watchify added to the toolchain

--- public/bundle.js
+++ public/bundle.js
@@ -2,13 +2,13 @@
 // Configuration data passed to setupGameboard() in tetris_API;
 
 module.exports = {
-    width: 10,
-    height: 20,
-    pixel: 15,
-    tempo: 1000,
-    stockLength: 3,
-    tetrominoHeight: "random",
-    colorPalette: "classic"
+  width: 15,
+  height: 20,
+  pixel: 15,
+  tempo: 1000,
+  stockLength: 3,
+  tetrominoHeight: "random",
+  colorPalette: "classic"
 };
 
 },{}],2:[function(require,module,exports){
@@ -456,92 +456,6 @@ module.exports = function(colorPalette) {
     };
 };
 },{"./pointHelpers":4,"./tetrominoTypes":9}],11:[function(require,module,exports){
-const gameBoard = require('./gameBoard');
-const tetris = require('./tetrisAPI')(gameBoard, update);
-const {
-    drawRectangularGrid,
-    drawSquare,
-    fill,
-    clear
-} = require('./helpers/canvasHelpers')
-
-const tetrisContainer = document.createElement('DIV');
-tetrisContainer.className = 'tetris-container';
-
-const scoreContainer = document.createElement('DIV');
-scoreContainer.className = 'score-container';
-
-const STOCK_CANVAS = document.createElement('CANVAS');
-STOCK_CANVAS.className = 'stock-canvas';
-STOCK_CANVAS.height = tetris.onCanvas.pixel * 5;
-STOCK_CANVAS.width = tetris.onCanvas.width;
-
-const CANVAS = document.createElement('CANVAS');
-CANVAS.className = 'large-canvas';
-CANVAS.height = tetris.onCanvas.height;
-CANVAS.width = tetris.onCanvas.width;
-
-document.querySelector('body').appendChild(tetrisContainer);
-tetrisContainer.appendChild(scoreContainer);
-tetrisContainer.appendChild(STOCK_CANVAS);
-tetrisContainer.appendChild(CANVAS);
-
-function score() {
-    scoreContainer.textContent = `SCORE: ${tetris.getState().score}`;
-}
-
-function update() {
-    score();
-    render();
-}
-
-function render() {
-    const tetromino = tetris.getState().tetrominoVertices;
-    const squares = tetris.getState().squareVertices;
-
-    clear(CANVAS);
-    fill(CANVAS, 'black');
-    if(tetromino) {
-        tetromino
-            .map(square => drawSquare(square, CANVAS, square[0].prop.color)
-            .fill()
-        );
-    };
-    if(squares) {
-        squares
-            .map(square => drawSquare(square, CANVAS, square[0].prop.color)
-            .fill()
-        );
-    };
-    drawRectangularGrid(CANVAS, tetris.getState().pixel, "white");
-};
-
-window.addEventListener('keydown', (e) => {
-    if(e.key === 'Enter' || e.key === ' ') {
-        !tetris.isGameRunning() ? tetris.startGame() : tetris.pauseGame();
-    } else if(e.key === 'ArrowDown' && tetris.isGameRunning()) {
-        tetris.moveDown();
-    } else if(e.key === 'ArrowLeft' && tetris.isGameRunning()) {
-        tetris.moveLeft();
-    } else if(e.key === 'ArrowRight' && tetris.isGameRunning()) {
-        tetris.moveRight();
-    } else if(e.key === 'a' || e.key === 'A' && tetris.isGameRunning()) {
-        tetris.turnRight();
-    } else if(e.key === 'z' || e.key === 'Z' && tetris.isGameRunning()) {
-        tetris.turnLeft();
-    };
-});
-
-update();
-
-
-
-
-
-
-
-
-},{"./gameBoard":1,"./helpers/canvasHelpers":3,"./tetrisAPI":13}],12:[function(require,module,exports){
 
 const { 
     movePointOnY,
@@ -672,7 +586,7 @@ function tetris(prevState, action, callback) {
 };
 
 module.exports = tetris;
-},{"./helpers/arrayHelpers":2,"./helpers/pointHelpers":4,"./helpers/tetrominoManipulation":7}],13:[function(require,module,exports){
+},{"./helpers/arrayHelpers":2,"./helpers/pointHelpers":4,"./helpers/tetrominoManipulation":7}],12:[function(require,module,exports){
 // Function returning object with all tetris actions;
 
 const tetris = require('./tetris');
@@ -747,4 +661,90 @@ module.exports = function (gameBoard, callback) {
         getState,
     };
 };
-},{"./helpers/setupGameboard":6,"./tetris":12}]},{},[11]);
+},{"./helpers/setupGameboard":6,"./tetris":11}],13:[function(require,module,exports){
+const gameBoard = require('./gameBoard');
+const tetris = require('./tetrisAPI')(gameBoard, update);
+const {
+    drawRectangularGrid,
+    drawSquare,
+    fill,
+    clear
+} = require('./helpers/canvasHelpers')
+
+const tetrisContainer = document.createElement('DIV');
+tetrisContainer.className = 'tetris-container';
+
+const scoreContainer = document.createElement('DIV');
+scoreContainer.className = 'score-container';
+
+const STOCK_CANVAS = document.createElement('CANVAS');
+STOCK_CANVAS.className = 'stock-canvas';
+STOCK_CANVAS.height = tetris.onCanvas.pixel * 5;
+STOCK_CANVAS.width = tetris.onCanvas.width;
+
+const CANVAS = document.createElement('CANVAS');
+CANVAS.className = 'large-canvas';
+CANVAS.height = tetris.onCanvas.height;
+CANVAS.width = tetris.onCanvas.width;
+
+document.querySelector('body').appendChild(tetrisContainer);
+tetrisContainer.appendChild(scoreContainer);
+tetrisContainer.appendChild(STOCK_CANVAS);
+tetrisContainer.appendChild(CANVAS);
+
+function score() {
+    scoreContainer.textContent = `SCORE: ${tetris.getState().score}`;
+}
+
+function update() {
+    score();
+    render();
+}
+
+function render() {
+    const tetromino = tetris.getState().tetrominoVertices;
+    const squares = tetris.getState().squareVertices;
+
+    clear(CANVAS);
+    fill(CANVAS, 'black');
+    if(tetromino) {
+        tetromino
+            .map(square => drawSquare(square, CANVAS, square[0].prop.color)
+            .fill()
+        );
+    };
+    if(squares) {
+        squares
+            .map(square => drawSquare(square, CANVAS, square[0].prop.color)
+            .fill()
+        );
+    };
+    drawRectangularGrid(CANVAS, tetris.getState().pixel, "white");
+};
+
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'Enter' || e.key === ' ') {
+        !tetris.isGameRunning() ? tetris.startGame() : tetris.pauseGame();
+    } else if(e.key === 'ArrowDown' && tetris.isGameRunning()) {
+        tetris.moveDown();
+    } else if(e.key === 'ArrowLeft' && tetris.isGameRunning()) {
+        tetris.moveLeft();
+    } else if(e.key === 'ArrowRight' && tetris.isGameRunning()) {
+        tetris.moveRight();
+    } else if(e.key === 'a' || e.key === 'A' && tetris.isGameRunning()) {
+        tetris.turnRight();
+    } else if(e.key === 'z' || e.key === 'Z' && tetris.isGameRunning()) {
+        tetris.turnLeft();
+    };
+});
+
+update();
+
+
+
+
+
+
+
+
+},{"./gameBoard":1,"./helpers/canvasHelpers":3,"./tetrisAPI":12}]},{},[13]);
---
+++ src/gameBoard.js
@@ -0,0 +1,11 @@
+// Configuration data passed to setupGameboard() in tetris_API;
+
+module.exports = {
+  width: 15,
+  height: 20,
+  pixel: 15,
+  tempo: 1000,
+  stockLength: 3,
+  tetrominoHeight: "random",
+  colorPalette: "classic"
+};
