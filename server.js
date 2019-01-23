1b1e8a365348c631b840ab137f5347a3c0ae83fd
MaciejReimann <maciej.reimann@gmail.com>
2019-01-23 18:06:28 +0100

    deploy again

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.static('public'));
+// Set static folder
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname, '/index.html');
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
